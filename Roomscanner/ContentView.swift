c9b5e0b7e529c2db357591184fbe18f43c62f90e
Mikael Deurell <mikael@deurell.net>
2022-07-13 11:31:50 +0200

    Initial Commit

---
+++ Roomscanner/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Roomscanner
+//
+//  Created by Mikael Deurell on 2022-07-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Roomscanner/RoomscannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomscannerApp.swift
+//  Roomscanner
+//
+//  Created by Mikael Deurell on 2022-07-13.
+//
+
+import SwiftUI
+
+@main
+struct RoomscannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
